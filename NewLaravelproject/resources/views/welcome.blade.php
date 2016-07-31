5359d17f9453953edccf1d83e54b4960215a69c8
ChetraYeang <chetra007.yeang@gmail.com>
2016-07-31 19:43:27 +0700

    Install laravelcollective/html and link css js
    this install for use form  and link css js of laravel 5.,2 but not
    testing can run or not

--- NewLaravelproject/resources/views/welcome.blade.php
+++ NewLaravelproject/resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <title>Laravel</title>
+        {!!html::style('https://fonts.googleapis.com/css?family=Lato:100')!!}
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
